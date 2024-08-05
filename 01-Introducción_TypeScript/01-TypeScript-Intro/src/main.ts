76a45a2164fc2057479b1f29c4c3c56b5fb48237
Carlos Martinez <cenriquemsrtinezc@gmail.com>
2024-08-05 05:55:21 -0500

    segundo dia

--- 01-Introducción_TypeScript/01-TypeScript-Intro/src/main.ts
+++ 01-Introducción_TypeScript/01-TypeScript-Intro/src/main.ts
@@ -2,16 +2,3 @@ import './style.css'
 /* import './topics/01-basic-types' */
 /* import './topics/02-object-interface' */
 import './topics/03-funtion-basic'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-
-
-`
-console.log('Hola mundo');
-
-const a: number = 10;
-
-console.log(a);
-
-// IMPORTANTE:
-// Ver la consola de JavaScript
--- 01-Introducción_TypeScript/01-TypeScript-Intro/src/topics/03-funtion-basic.ts
+++ 01-Introducción_TypeScript/01-TypeScript-Intro/src/topics/03-funtion-basic.ts
@@ -3,13 +3,29 @@
 /* en las funciones despues del addNumber(): este dos puntos significa el valor de retorno 
 por defecto typescript define que un void que significa no retorna nada. */
 
-function addNumbers() {
+function addNumbers(a:number , b:number) {
+    return a+ b
+}
+addNumbers(1, 2)
 
-
-
-    
+const addNumbersArrow = (a: number, b: number): string => {   /* en una funcion flecha  se puede especificar el resultado que devuelve 
+    si se cambia a string nos da un error porque la firma de la función sabe que es un numero  */
+    return `${a + b}`  /* agregando `` se cambia a ser un to.string es decir pasa de ser un numero a ser un tecto */
 }
 
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2) { /* se aconseja que lo primero es colocar los valores obligatorios y despues secundarios ypor ultimo el de valor por defecto */
+    return firstNumber * base
+}
+
+
+
+// const result: number = addNumbers(1,2)
+// console.log({ result })
+// const result2: string = addNumbersArrow(1,2)
+// console.log({result2})
+/* const multiplyResult: number = multiply (5)
+console.log({multiplyResult})
+ */
 
 
 
